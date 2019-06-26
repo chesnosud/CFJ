16b7a0b030267b3465b2e598413a565879d56fc0
hp0404 <39014459+hp0404@users.noreply.github.com>
2019-06-26 16:28:20 +0300

    shortened

--- fired.py
+++ fired.py
@@ -18,14 +18,12 @@ def filter_data(dataframe):
     df['id'] = 'xxxxx'
     df['Суд'] = 'xxxxx'
     df = df[['id', 'Піб', 'Суд', 'Дата прийняття', 'Link', 'Назва документу']]
-    df
     return df[::-1]
 
 def add_info(df):
     """З тексту отримую назву суду та підставу для звільнення."""
 
     container = []
-    reason = []
     for i in df['Назва документу']:
         try:
             m = re.search(r'(\w+[^\s]\w+\s\w+\s\суду\s\w+\s\w+)', i).group(1)
@@ -33,22 +31,15 @@ def add_info(df):
             m = re.search(r'(\w+[^\s]\w+\s+\w+\s+\w+[^s+]суду)', i).group(1)
 
         container.append(m)  
-        reason.append(re.search(r'(\w+\s+\w+\s+\w+$)', i).group(1))
-        
+
     df['Суд'] = container
 
-    n = []
-    for i in reason:
-        if 'у відставку' in i:
-            n.append(' '.join(i.split(' ')[1:]))
-        else:
-            n.append(i)
-    df['Підстава для звільнення'] = n
-    
+    reason = [ re.search(r'(\w+\s+\w+\s+\w+$)', i).group(1) for i in df['Назва документу'] ] 
+    df['Підстава для звільнення'] = [' '.join(i.split(' ')[1:]) if 'у відставку' in i else i for i in reason]
     return df[['id', 'Піб', 'Суд', 'Дата прийняття', 'Link', 'Підстава для звільнення']]
 
 if __name__ == "__main__":
     df = requests_beautifulsoup()
     df = filter_data(df)
     df = add_info(df)
-    df.to_excel(f"zvilnenii.xlsx", sheet_name=f'test_26.06.2019')
+    df.to_excel(f"results/zvilnenii.xlsx", sheet_name=f'test_26.06.2019')
