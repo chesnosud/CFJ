94bcf2dcd977800418b753071190efbf335211f5
hp0404 <39014459+hp0404@users.noreply.github.com>
2019-06-29 10:06:25 +0300

    додав try/except

--- kvalif.py
+++ kvalif.py
@@ -12,7 +12,7 @@ def kvalif(url: str, file_name: str):
     soup = BeautifulSoup(r.text, 'html.parser')
     lh_container = LH.fromstring(soup.prettify())
     date_string = lh_container.xpath('/html/body/div[1]/div/div[2]/div/div/div/div[1]/div/p[1]/text()')
-    text = ''.join([ i for i in date_string ])
+    text = ' '.join([ i for i in date_string ])
     pattern = r'([0-9]{2}.+[0-9]{4})'
     date = re.search(pattern, text).group(1)
     
@@ -21,14 +21,21 @@ def kvalif(url: str, file_name: str):
     df.columns = ['ПІБ', 'Суд', 'К-сть балів', 'Результат']
     df['Дата кваліфоцінювання'] = date
     df['Результат'] = [ i.split('.')[0] for i in list(df['Результат']) ]
-    df['Чи є профайл'] = "123"
-    df['Порушення доброчесності'] = "123"
-    df['Дата відправки до ВККС'] = "123"
+    df['Чи є профайл'] = " "
+    df['Порушення доброчесності'] = " "
+    df['Дата відправки до ВККС'] = " "
     df = df[['Дата кваліфоцінювання', 'ПІБ', 'Суд', 'Чи є профайл', 'Порушення доброчесності', 'Дата відправки до ВККС', 'Результат']]
-    df.to_excel(f"{file_name}.xlsx", sheet_name=f'{date}')
+    df.to_excel(f"{file_name}.xlsx", sheet_name=f'{date}', index=False)
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    url = args[0]
-    file_name = args[1]
-    kvalif(url=url, file_name=file_name)
+    try:
+        url, file_name = (args[0], args[1])
+        kvalif(url=url, file_name=file_name)
+    except:
+        print("""Були задані хибні аргументи
+        Перший аргумент: url посилання на новину ВККС
+        Другий агрумент: назва файлу для збереження результатів
+        Де аргументи задані через один пробіл
+        Наприклад, "python ./kvalif.py https://vkksu.gov.ua/ua/news/xxx kvalif_excel"
+        """)
