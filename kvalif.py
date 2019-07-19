fc6c5b6f669a4cc79cd72b89d772533843cd696f
hp0404 <39014459+hp0404@users.noreply.github.com>
2019-07-19 21:25:01 +0300

    final version

--- kvalif.py
+++ kvalif.py
@@ -1,26 +1,25 @@
-import sys, re
-import requests
+import requests, re, sys
 import pandas as pd
-import lxml.html as LH
+from lxml.html import fromstring
 from bs4 import BeautifulSoup
 
 def parse_kvalif(url: str, file_name: str):
     """Парсер результатів кваліфоцінювання."""
     
-    # Отримую дату проведення кваліфу за допомогою regex
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
-    lh_container = LH.fromstring(soup.prettify())
+    lh_container = fromstring(soup.prettify())
     date_string = lh_container.xpath('/html/body/div[1]/div/div[2]/div/div/div/div[1]/div/p[1]/text()')
     date = re.search(r'([0-9]{1,2}\s+\w+\s+[0-9]{4})', ' '.join(date_string)).group(1)
     
-    # Отримую таблицю з результатами та створюю нові колонки
     df = pd.read_html(soup.prettify(), skiprows=1)[0]
     df.columns = ['ПІБ', 'Суд', 'К-сть балів', 'Результат']
     df['Дата кваліфоцінювання'] = date
-    df['Результат'] = [ i.split('.')[0] for i in df['Результат'] ]
+    df['Результат'] = [ i.split('.')[0] for i in df['Результат'].values.tolist() ]
     df['Чи є профайл'], df['Порушення доброчесності'], df['Дата відправки до ВККС']  = " ", " ", " "
-    df = df[['Дата кваліфоцінювання', 'ПІБ', 'Суд', 'Чи є профайл', 'Порушення доброчесності', 'Дата відправки до ВККС', 'Результат']]
+    df['Область'] = [i.split()[-2:] for i in df['Суд'].values.tolist()]
+    df['Область'] = df['Область'].str.join(' ')
+    df = df[['Дата кваліфоцінювання', 'ПІБ', 'Область', 'Чи є профайл', 'Порушення доброчесності', 'Дата відправки до ВККС', 'Результат']]
     df.to_excel(f"{file_name}.xlsx", sheet_name=f'{date}', index=False)
 
 if __name__ == "__main__":
@@ -33,5 +32,5 @@ if __name__ == "__main__":
         Перший аргумент: url посилання на новину ВККС
         Другий агрумент: назва файлу для збереження результатів
         Де аргументи задані через один пробіл
-        Наприклад, "python ./kvalif.py https://vkksu.gov.ua/ua/news/xxx kvalif_excel"
+        Наприклад, "python ./kvalif.py https://vkksu.gov.ua/ua/news/xxx 19-липня"
         """)
