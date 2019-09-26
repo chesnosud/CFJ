c63f9b44cb0e0529e46db06116731a85b47414bc
hp0404 <39014459+hp0404@users.noreply.github.com>
2019-09-26 12:14:19 +0300

    Add files via upload

--- concat_files.py
+++ concat_files.py
@@ -1,20 +1,20 @@
 import os, datetime
 import pandas as pd
 
-def concat_files(folder='fired'):
-    """
-    Об'єднує усі файли в один
+
+def concat_files(folder="fired") -> None:
+    """    Об'єднує усі файли з розширенням .xlsx в межах однієї папки
 
     folder: папка із файлами
     """
-    
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), folder)
 
-    files = [pd.read_excel(os.path.join(path, f)) for f in os.listdir(path) if f.endswith('.xlsx')]
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), folder)
+    files = [pd.read_excel(os.path.join(path, f)) for f in os.listdir(path) if f.endswith(".xlsx")]
+
     df = pd.concat(files, ignore_index=True).drop_duplicates()
-    
-    name = f'combined_asof_{str(datetime.datetime.now().date())}.xlsx'
+    name = f"combined_asof_{str(datetime.datetime.now().date())}.xlsx"
     df.to_excel(os.path.join(path, name), index=False)
+
 
 if __name__ == "__main__":
     concat_files()
--- kvalif.py
+++ kvalif.py
@@ -1,4 +1,6 @@
-import re, sys
+import sys
+import os
+import re
 
 import requests
 from lxml.html import fromstring
@@ -6,45 +8,54 @@ from bs4 import BeautifulSoup
 
 import pandas as pd
 
+from concat_files import concat_files
 
-def parse_kvalif(url: str, file_name: str):
-    """Парсер результатів кваліфоцінювання."""
 
+def parse_kvalif(url: str) -> None:
+    """ Парсить html таблицю з результати оцінювання
+    
+    url: посилання на новину 
+    """
+
+    # Отримання вмісту сторінки за url посиланням
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "lxml")
-    lh_container = fromstring(soup.prettify())
-    date_string = lh_container.xpath(
+
+    # Отримання дати оцінювання за xpath тексту сторінки
+    date_string = fromstring(soup.prettify()).xpath(
         "/html/body/div[1]/div/div[2]/div/div/div/div[1]/div/p[1]/text()"
     )
     date = re.search(r"(\d{1,2}\s+\w+\s+\d{4})", " ".join(date_string)).group(1)
 
+    # Зчитування html таблиці з результатами оцінювання
     df = pd.read_html(soup.prettify(), skiprows=1)[0]
     df.columns = ["ПІБ", "Суд", "К-сть балів", "Результат"]
 
-    df["Дата кваліфоцінювання"] = date
-    df["Результат"] = df["Результат"].str.split(".").str[0]
-    df["Область"] = df["Суд"].str.split().str[-2:].str.join(" ")
-
+    # Редагування таблиці 
+    df["Дата кваліфоцінювання"] = date # додана дата оцінювання
+    df["Результат"] = df["Результат"].str.split(".").str[0] # скорочений результат
+    df["Область"] = df["Суд"].str.split().str[-2:].str.join(" ") # область з назви суду
     df["Чи є профайл"], df["Порушення доброчесності"], df["Дата відправки до ВККС"] = (
-        "xxx",
-        "xxx",
-        "xxx",
+        "xxx", "xxx", "xxx",
     )
     df = df[
         [
-            "Дата кваліфоцінювання",
-            "ПІБ",
-            "Область",
-            "Чи є профайл",
-            "Порушення доброчесності",
-            "Дата відправки до ВККС",
-            "Результат",
+            "Дата кваліфоцінювання", "ПІБ", "Область", "Чи є профайл",
+            "Порушення доброчесності", "Дата відправки до ВККС", "Результат",
         ]
     ]
-    df.to_excel(f"{file_name}.xlsx", sheet_name=f"{date}", index=False)
+    
+    # створення папки, в яку буде збережений результат
+    os.makedirs("./результати_кваліфоцінювання", exist_ok=True)
+    # збереження таблиці в xlsx форматі
+    df.to_excel(
+        f"./результати_кваліфоцінювання/{date}_результат.xlsx", 
+        sheet_name=f"{date}", index=False)
 
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    url, file_name = (args[0], args[1])
-    parse_kvalif(url=url, file_name=file_name)
+
+    parse_kvalif(url=sys.argv[1])
+    
+    # об'єднує усі завантажені в папку файли в один
+    concat_files(folder="результати_кваліфоцінювання")
