313d5f544e2b5d9acbef4d5a67a5c3133de0544a
hp0404 <clarencesexual@gmail.com>
2020-06-08 11:19:20 +0300

    fixed #6

--- chesnosud/declarations.py
+++ chesnosud/declarations.py
@@ -10,14 +10,18 @@ from typing import Any, Dict, Iterable, List
 PATH = Path(__file__).resolve().parent.parent / "data" / "processed" / "declarations"
 
 
-def count_things(data: List[Any], value_type: str = "gifts") -> Iterable[int]:
+def count_things(
+    data: Dict[str, Any], 
+    value_type: str = "gifts"
+) -> Iterable[Any]:
     """ Перевіряє наявність "одиниці" в декларації та повертає розмір кожної. 
     
 
     Parameters
     ----------
-    data : list of nested dictionaries (requests' response)
-        Є аргументом генератора `step`.   
+    data : Dict[str, Any]
+        requests' response: unified_source
+        
     value_type : str
         Тип об'єкта, який слід перевірити (sizeIncome, model, totalArea)
     """
@@ -29,23 +33,22 @@ def count_things(data: List[Any], value_type: str = "gifts") -> Iterable[int]:
             yield item.get(value_type, 0)
             
             
-def step(data: List[Any]) -> Iterable[Dict[str, int]]:
-    """ Збирає інформацію, що стосується (не)рухомого майна та подарунків судді.
+def step(data: List[Dict[str, Any]]) -> Iterable[Dict[str, Any]]:
+    """ Збирає інформацію, що стосується (не)рухомого майна та подарунків декларанта.
     
 
     Parameters
     ----------
-    data : list of nested dictionaries (requests' response)
-        Відповідь declarations.com API
+    data : List[Dict[str, Any]]
+        requests' response: object_list and raw_source
 
 
     Notes
     -----
     Expected exceptions:
     KeyError
-        Коли ключ відсутній (відсутнє майно або подарунки), повертає 0;    
+        Коли ключ відсутній (відсутнє майно або подарунки), повертає 0 або "";    
      """
-
     for item in data:
 
         step = item["unified_source"]
@@ -80,68 +83,55 @@ def step(data: List[Any]) -> Iterable[Dict[str, int]]:
             "gift_num": len(gifts) if gifts else 0,
             "gift_sum": sum(gifts) if gifts else 0,
         }
+        
+        
+def infocard(data: List[Dict[str, Any]]) -> Iterable[Dict[str, Any]]:
+    """ Збирає інформацію, що стосується основний відомостей про декларанта
+    
+    
+    Parameters
+    ----------
+    data : List[Dict[str, Any]]
+        requests' response: infocard
+    """
+    for item in data:
+        infocard = item["infocard"]
+        name = f"{infocard['last_name']} {infocard['first_name']} {infocard['patronymic']}"
+        
+        yield {
+            "name": name,
+            "document_type": infocard["document_type"],
+            "created_date": infocard["created_date"],
+            "declaration_year": infocard["declaration_year"]
+        }
+    
 
-
-def create_dataframe(data: List[Any]) -> pd.DataFrame:
+def create_dataframe(
+    data: List[Dict[str, Any]], 
+    name: str
+) -> pd.DataFrame:
     """Створює таблицю на основі напівструктурованих даних АРІ declarations.com.ua
     
     
     Parameters
     ----------
-    data : list of nested dictionaries (requests' response)
-        Відповідь declarations.com API.
+    data : List[Dict[str, Any]]
+        requests' response: object_list
+    
+    name: str
+        ПІБ декларанта; використовується верифікації декларанта.
     
     
     Notes
     -----
-    Основа таблиці формується з поля `infocard`, додаткові відомості - майно, 
-    подарунки, лінки - з полів `unified_source` та `raw_source` відповідно. 
+    Основа таблиці формується з поля infocard, додаткові відомості - майно, 
+    подарунки, лінки - з полів unified_source та raw_source відповідно. 
     """
-
-    df = pd.concat(
-        [pd.json_normalize(item["infocard"]) for item in data], ignore_index=True
-    )
-    meta_data = pd.DataFrame(step(data))
-
-    return df.join(meta_data)
-
-
-def reshape_dataframe(dataframe: pd.DataFrame, name: str) -> pd.DataFrame:
-    """Перероблює таблицю:
-        (а) створює нові та прибирає зайві колонки;
-        (б) верифікує декларанта;
-        (в) позбувається 'Форм змін'
-        
-        
-    Parameters
-    ----------
-    dataframe : pd.DataFrame
-        Таблиця, отримана з create_dataframe().
-    name: str
-        ПІБ декларанта; використовується верифікації декларанта.
-    """
-
-    df = dataframe.copy()
-
-    df["name"] = (
-        df["last_name"]
-        .str.cat(df["first_name"], sep=" ")
-        .str.cat(df["patronymic"], sep=" ")
-    )
-
-    df = df.loc[(df["name"].eq(name)) & (df["document_type"].ne("Форма змін"))]
-
-    useless_columns = [
-        "first_name",
-        "last_name",
-        "patronymic",
-        "id",
-        "is_corrected",
-        "position",
-        "url",
-    ]
-
-    return df.drop(useless_columns, axis=1)
+    table = pd.DataFrame(infocard(data))
+    metadata = pd.DataFrame(step(data))
+    df = table.join(metadata)
+    
+    return df.loc[(df["name"].eq(name)) & (df["document_type"].ne("Форма змін"))]
 
 
 def rm_datedups(dataframe: pd.DataFrame) -> pd.DataFrame:
@@ -169,21 +159,18 @@ def rm_datedups(dataframe: pd.DataFrame) -> pd.DataFrame:
             created_date	    declaration_year	link
     5	2019-03-26	            2018	            d354c
     """
-
     df = dataframe.copy()
-
     df["created_date"] = pd.to_datetime(
         df["created_date"], format="%Y-%m-%d", errors="coerce"
     )
-
     df["declaration_year"] = df["declaration_year"].astype(int)
+    
     df = df.loc[~(df["declaration_year"].eq(2015) & df["link"].str.contains("/static"))]
-
     df = df.sort_values("created_date").drop_duplicates("declaration_year", keep="last")
 
     df["link"] = df["link"].str.replace(
-        pat="https://public-api.nazk.gov.ua/v1/declaration/",
-        repl="https://public.nazk.gov.ua/declaration/",
+        "https://public-api.nazk.gov.ua/v1/declaration/",
+        "https://public.nazk.gov.ua/declaration/",
     )
 
     return df.sort_values("declaration_year")
@@ -200,7 +187,6 @@ def save(df: pd.DataFrame, name: str) -> None:
     name : str
         ПІБ декларанта, яке використовується як назва файлу
     """
-
     _columns = {
         "paste": "Скопіювати", 
         "real_estate": "Нерухоме майно",
@@ -224,22 +210,20 @@ def collect_declarations(names: List[str], verbose : bool = False) -> Iterable[s
     names: List[str]
         Перелік ПІБ, чиї декларації слід зібрати
     """
-    
-    
     for i, person in reversed(list(enumerate(names))):
         person = person.strip().partition(".")[0]
         url = f"http://declarations.com.ua/search?q={person}+AND+суддя&deepsearch=on&format=opendata"
 
         try:
             r = requests.get(url, sleep(0.5)).json()
-            table = create_dataframe(r["results"]["object_list"])
-            reshaped_df = reshape_dataframe(table, person)
-            frame = rm_datedups(reshaped_df)
+            data = r["results"]["object_list"]
+            table = create_dataframe(data, person)
+            result = rm_datedups(table)
 
-            if frame.empty:
+            if result.empty:
                 raise ValueError
             else:
-                save(frame, person)
+                save(result, person)
 
         except ValueError:
             yield person
@@ -253,7 +237,6 @@ def collect_declarations(names: List[str], verbose : bool = False) -> Iterable[s
 @click.option("--verbose", "-v", is_flag=True, help="Print more output.")
 def cli(input_path, verbose):
     """ Download declarations for given names. """
-    
     declarants = pd.read_excel(input_path)
     names = declarants["names"].to_list()
     failed = list(collect_declarations(names, verbose))
